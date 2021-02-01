946d4fc7d4fc802c346a34eec4a459e336144cb1
Irene <iragso27@gmail.com>
2021-02-01 11:15:07 +0100

    js - correct

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,68 @@
 'use strict'
 
-let okWords = ['CACA', 'CASI', 'CAMA', 'CATA', 'CASA', 'SISA', 'TARTA', 'MAMA','MATAR', 'MATE', 'MATA', 'MASA', 'TEMA', 'TETE', 'TETA', 'TATA','TASA', 'FORMA', 'SACA', 'CATETA'];
+var okWords = ['CACA', 'CASI', 'CAMA', 'CATA', 'CASA', 'SISA', 'TARTA', 'MAMA','MATAR', 'MATE', 'MATA', 'MASA', 'TEMA', 'TETE', 'TETA', 'TATA','TASA', 'FORMA', 'SACA', 'CATETA'];
 
-let part = document.querySelectorAll('.part');
-let result = document.getElementById('result');
+var part = document.querySelectorAll('.part');
+var result = document.getElementById('result');
 var screenValue ='';
-let correct = document.getElementById('correct');
-let incorrect = document.getElementById('incorrect');
+var correct = document.getElementById('correct');
+var incorrect = document.getElementById('incorrect');
+var validator = document.getElementById('validator');
+
 
 
 for(var i = 0; i < part.length; i++) {
     part[i].addEventListener("click", partClick(i));
 }
 
+validator.addEventListener('click', btnValidate, false);
+
+function partClick(i) {
+    
+    return function() {
+        console.log("Has clicado en el numero " + i);
+        let valor = part[i].textContent;
+
+        result.innerHTML += valor;
+
+        screenValue = result.textContent;
+        console.log(screenValue);     
+    };
+    
+}
+
+function btnValidate() { 
+    
+    for (var z = 0; z < okWords.length; z++) {
+      
+        if (screenValue === okWords[z]) {
+            
+            correct.style.display = 'block';
+
+            setTimeout(() => {
+                location.reload(2000);
+            }, 1300);  
+            
+        } else {
+           
+            setTimeout(() => {
+                location.reload(2000);
+            }, 1300);  
+            
+
+        }
+    }  
+
+}
+
+
+
+/*
+for(var i = 0; i < part.length; i++) {
+    part[i].addEventListener("click", partClick(i));
+}
+*/
+/*
 function partClick(i) {
     
     return function() {
@@ -46,11 +96,8 @@ function partClick(i) {
                     incorrect.style.display = 'block';
                 }, 1000);    
                 
-                /*
-                setTimeout(() => {
-                    location.reload(2000);
-                }, 1300); 
-                */  
+                //setTimeout(() => {location.reload(2000);}, 1300); 
+                 
             }
 
         }  
@@ -59,3 +106,4 @@ function partClick(i) {
     };
     
 }
+*/
